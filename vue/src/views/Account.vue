dfd46cfbfc979c05017e24f56efec08645311829
Michal Kruzolek <michal.kruzolek@fdmgroup.com>
2024-04-16 20:00:10 +0200

    first commit

--- vue/src/views/Account.vue
+++ vue/src/views/Account.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="wrapper">
-        <breadcrumbs></breadcrumbs>
-        <h1>Moje konto</h1>
-    </div>
+  <div class="wrapper">
+    <breadcrumbs></breadcrumbs>
+    <h1>Moje konto</h1>
+    <h2>teścik gita</h2>
+  </div>
 </template>
