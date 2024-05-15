5244dcc66507556d1bd7bf48b824f21e32c2d092
235661 <235661@student.uek.krakow.pl>
2024-05-15 22:45:52 +0200

    fetch order history

--- vue/src/views/history.vue
+++ vue/src/views/history.vue
@@ -18,6 +18,23 @@
 
       <main>
         <h1>Historia zamówień</h1>
+
+        <ul class="history">
+          <li class="order" v-for="(order, index) in orders" :key="index">
+            <h4 class="title">{{ order.products.length }} sałatki</h4>
+            <p>Data zamówienia: {{ order.date }}</p>
+            <p>{{ calculateTotalPrice(order.products) }} zł</p>
+            <ul class="products-list" v-for="(product, productIndex) in order.products" :key="productIndex">
+              <li class="product">
+                <div class="product-header">
+                  <h6 class="title">{{ product.title }}</h6>
+                  <span class="price">{{ product.price.toFixed(2) }} zł</span>
+                </div>
+                <p class="description">{{ product.shortDescription }}</p>
+              </li>
+            </ul>
+          </li>
+        </ul>
       </main>
     </div>
   </div>
@@ -26,6 +43,7 @@
 <script setup>
 import { useRouter } from 'vue-router';
 import { getAuth, signOut } from 'firebase/auth';
+import {API_URL} from '@src/main.js'
 
 const router = useRouter();
 const auth = getAuth();
@@ -41,3 +59,42 @@ const logout = () => {
 };
 </script>
   
+<script>
+const clientId = localStorage.getItem('uid');
+
+const calculateTotalPrice = (products) => {
+    // Sprawdzamy, czy tablica produktów jest pusta
+    if (!Array.isArray(products) ||!products.length) {
+      return 0; // Zwracamy 0, jeśli tablica jest pusta lub nie jest tablicą
+    }
+
+    // Używamy reduce do obliczenia sumy cen
+    return products.reduce((accumulator, product) => {
+      // Sprawdzamy, czy produkt ma własność price
+      if ('price' in product && typeof product.price === 'number') {
+        return accumulator + product.price; // Dodajemy cenę produktu do akumulatora
+      }
+      return accumulator; // Jeśli produkt nie ma ceny, zwracamy akumulator bez zmian
+    }, 0); // Akumulator startuje od 0
+  }
+
+export default {
+    data() {
+        return {
+            orders: [],
+        };
+    },
+    async created() {
+        try {
+            const response = await fetch(API_URL + '/orderHistory/' + clientId);
+            if (!response.ok) {
+                throw new Error('API response was not ok');
+            }
+            const data = await response.json();
+            this.orders = data;
+        } catch (error) {
+            console.error('Problem z pobieraniem danych historii: ', error)
+        }
+    }
+}
+</script>
